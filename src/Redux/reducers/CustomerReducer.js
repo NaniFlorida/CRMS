b43ac73f8917b4da540c92871b11d36a9bf3e803
Venkatesh Peddapeta <peddapetavenkatesh@gmail.com>
2022-01-03 21:16:25 +0530

    final change

--- src/Redux/reducers/CustomerReducer.js
+++ src/Redux/reducers/CustomerReducer.js
@@ -13,25 +13,7 @@ function CustomerReducer(customers = initialState, action) {
     switch (type) {
         case CustomerConstants.GET_CUSTOMERS:
             return payload;
-
-        // case CustomerConstants.ADD_CUSTOMERS:
-        //     return [...customers, payload]
-
-        // case CustomerConstants.UPDATE_CUSTOMERS:
-        //     return customers.map((customer) => {
-        //         if (customer.id === payload.id) {
-        //             return {
-        //                 ...customer,
-        //                 ...payload
-        //             }
-        //         } 
-        //         else {
-        //             return customer
-        //         }
-        //     })
-
-        // case CustomerConstants.REMOVE_CUSTOMERS:
-        //     return customers.filter(s => s.id !== payload)
+            
         case CustomerConstants.ADD_CUSTOMERS:
            return payload;
 
